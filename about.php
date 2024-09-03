d7803a96ee3aa0ae06f495b4ca613be03d6c9178
aaryan777raj <138811020+aaryan777raj@users.noreply.github.com>
2024-09-03 21:15:58 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+    <section class="header">
+        <a href="home.php" class="logo">travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+
+
+
+
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+
+
+    <div class="heading" style="background:url(./images/all2.jpg) no-repeat">
+        <h1></h1>
+
+    </div>
+
+    <section class="about">
+        <div class="image">
+            <img src="./images/about us.jpg" alt="image">
+
+
+        </div>
+
+        <div class="content">
+            <h3>ABOUT US </h3>
+            <p>I am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience. I like to read, I like to write; I like ..</p>
+            <p>I am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience. I like to read, I like to write; I like ..</p>
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>top destinations</span>
+                </div>
+            </div>
+
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>best price</span>
+                </div>
+            </div>
+
+
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>24/7 </span>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+    </section>
+
+
+<!-- review start-->
+
+<section class="review">
+    <div class="swiper reviews-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                </div>
+                <p></p>
+                <h3>monty lalu</h3>
+                <span>traveller</span>
+                
+
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                    
+                </div>
+                <p></p>
+                <h3>moonu prasad</h3>
+                <span>traveller</span>
+                
+                
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                    
+                </div>
+                <p></p>
+                <h3>lalu</h3>
+                <span>traveller</span>
+                
+                
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                    
+                </div>
+                <p></p>
+                <h3>daruram</h3>
+                <span>traveller</span>
+                
+                
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                    
+                </div>
+                <p></p>
+                <h3>zimi</h3>
+                <span>traveller</span>
+                
+                
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                </div>
+                <p></p>
+                <h3>surovi</h3>
+                <span>traveller</span>
+                
+                
+            </div>
+
+            <div class="swiper-slider slide">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    
+                    
+                </div>
+                <p></p>
+                <h3>swaalina</h3>
+                <span>traveller</span>
+                
+                
+            </div>
+
+
+
+
+
+
+
+        </div>
+        
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quicks Links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+
+            </div>
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask question</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+
+
+
+            </div>
+
+            <div class="box">
+                <h3>contacts info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+91-7853453456</a>
+                <a href="#"><i class="fas fa-phone"></i>+91-8766788765</a>
+                <a href="#"><i class="fas fa-envelope"></i>abc@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>ranchi, india - 834001</a>
+
+            </div>
+
+            <div class="box">
+                <h3>follows us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin/a>
+
+
+
+            </div>
+
+
+
+
+
+        </div>
+
+        <div class="credit">created by <span>Mr. Aaryan Raj</span> |All Rigits Reserved. </div>
+
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <script src="./script.js"></script>
+
+</body>
+
+</html>
---
+++ package.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Packages</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+    <section class="header">
+        <a href="home.php" class="logo">travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+
+
+
+
+        </nav>
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+
+
+    <div class="heading" style="background:url(./images/all2.jpg) no-repeat">
+        <h1>Packages</h1>
+
+    </div>
+
+
+
+
+    <section class="packages">
+        <h1 class="heading-titles">top destination<h1>
+
+        <div class="box-container">
+            <div class="box">
+                <div class="image">
+                    <img src="./images/dubai.jpg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>dubai</h3>
+                    <h2>Explore the city of sky highs</h2>
+                    
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+
+
+            <div class="box">
+                <div class="image">
+                    <img src="./images/paris.webp" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Paris</h3>
+                    
+                    <p>Explore the place of Romance</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            
+            <div class="box">
+                <div class="image">
+                    <img src="./images/shanghai.jpeg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Shanghai</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the place of colors</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/japan.jpg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Japan</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Timeless Beauty </p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/nepal.webp" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Nepal</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Majestic Peaks </p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/taj.jpg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Agra</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Eternal Wonders</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/australia.jpeg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>australia</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Boundless Adventures</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/london.jpg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>London</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Timeless Charm</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/mexico.jpg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Mexico</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Vibrant Culture</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/mosco.webp" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Moscow</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Majestic Heritage</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/rome.webp" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Rome</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Timeless Splendor</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+            <div class="box">
+                <div class="image">
+                    <img src="./images/amsetdam.jpg" alt="">
+
+                </div>
+                <div class="content">
+                    <h3>Amsterdam</h3>
+                    <h2>Adventure & tour</h2>
+                    <p>Explore the Place of Enchanting Canals</p>
+                    <a href="book.php" class="btn">book now</a>
+
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
+
+        <div class="load-more"><span class="btn">Load more</span></div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quicks Links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+
+            </div>
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask question</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+
+
+
+            </div>
+
+            <div class="box">
+                <h3>contacts info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+91-7853453456</a>
+                <a href="#"><i class="fas fa-phone"></i>+91-8766788765</a>
+                <a href="#"><i class="fas fa-envelope"></i>abc@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>ranchi, india - 834001</a>
+
+            </div>
+
+            <div class="box">
+                <h3>follows us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin/a>
+
+
+
+            </div>
+
+
+
+
+
+        </div>
+
+        <div class="credit">created by <span>Mr. Aaryan Raj</span> |All Rigits Reserved. </div>
+
+
+
+
+
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <script src="./script.js"></script>
+
+</body>
+
+</html>
